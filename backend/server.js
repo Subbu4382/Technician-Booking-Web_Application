96ff50af4b32fda83179138c7302174a28285935
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-12-26 10:47:57 +0530

    Technician Booking Web application

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(" MongoDB Atlas Connected Successfully");
-  } catch (error) {
-    console.error(" MongoDB Connection Failed:", error.message);
-    process.exit(1);
-  }
-};
-
+const connectDB = require("./config/db");
 connectDB();
 
 // REGISTER ALL ROUTES
